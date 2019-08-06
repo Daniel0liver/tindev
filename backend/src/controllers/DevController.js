512d0972a8bdde83534abe42359fc9d37b539ba1
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-08-06 14:43:55 -0300

    Listing users
    Listing all but the logged in user, the ones he has already given like the one he has already given dislike

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,15 +1,31 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 module.exports = {
+  async index(req, res){
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } }, // $ne é como se fosse ( != ), ou seja, liste todos os usuarios menos o usuario logado
+        { _id: { $nin: loggedDev.likes } }, // $nin é o Not in, ou seja, liste todos os usuarios que o usuario logado ja deu like
+        { _id: { $nin: loggedDev.dislikes } },
+      ],
+    })
+
+    return res.json(users);
+  },
+
   async store(req, res){
     const { username } = req.body;
 
     const userExists = await Dev.findOne({ user: username });
-
+    // Validando se o usuário ja existe na base de dados
     if(userExists){
       return res.json(userExists);
     }
-    
+    // Pegando os dados dos usuários na api do github atraves do username
     const response = await axios.get(`https://api.github.com/users/${username}`);
 
     const { name, bio, avatar_url: avatar } = response.data;
@@ -24,3 +40,13 @@ module.exports = {
     return res.json(dev);
   }
 };
+
+/*
+Boas praticas:
+  Seu controller não deve mais do que 5 metodos essencias
+    - Index
+    - Show
+    - Store
+    - Update
+    - Delete
+*/
