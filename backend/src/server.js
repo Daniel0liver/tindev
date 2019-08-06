c1d0487152afea1edd04a43af7a6a87c663d6677
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-08-06 12:40:36 -0300

    Connecting mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
 // Metodos Principais: GET, POST, PUT, DELETE
+mongoose.connect('mongodb+srv://omnistack:daniel1812@cluster0-f7unl.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
 
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
+
+// M - Model, V - View, C - Controller
