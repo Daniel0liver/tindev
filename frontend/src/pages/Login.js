deba3cdd644209019360a4e69281b752e159a0e0
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-08-08 11:14:44 -0300

    styling the login page
    - adding fivicon and icon in the input

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -7,33 +7,39 @@ import logo from '../assets/logo.svg'
 
 export default function Login({ history }) {
   const [username, setUsername] = useState(''); // useState retorna dois valores: username e um função
-  
-  async function HandSubmit(e){
+
+  async function HandSubmit(e) {
     e.preventDefault(); // Bloqueia o eventDefault(Redirecionar para outra pagina) de um form
-    
+
     const response = await api.post('/devs', {
       username,
     });
 
     const { _id } = response.data;
-    
+
     history.push(`/dev/${_id}`);
-    
+
   }
 
-  return(
+  return (
     <div className="login-container">
-      <form onSubmit={HandSubmit}>
-        <img src={logo} alt="Tindev" />
-        <input 
-        placeholder="Digite seu usuário do GitHub" 
-        value={username}
-        onChange={e => setUsername(e.target.value)} // onChange returna um event 
-        />
-        <button type="submit">Enviar</button>
-      </form>
+      <div className="login-content">
+        <form onSubmit={HandSubmit}>
+          <img src={logo} alt="Tindev" />
+          <div class="ui left icon input">
+            <input
+              placeholder="Usuário GitHub"
+              value={username}
+              onChange={e => setUsername(e.target.value)} // onChange returna um event 
+            />
+            <i aria-hidden="true" class="users icon"></i>
+          </div>
+
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
     </div>
   );
-} 
+}
 
 // O state de um component é toda aquela informação que ele vai manipular
