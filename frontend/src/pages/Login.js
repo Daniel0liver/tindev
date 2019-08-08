5cd58744eb05b5e9ecb7ad75a161641b82d71d0e
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-08-07 21:43:11 -0300

    creating the login page and styling

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
 
 import  logo from '../assets/logo.svg'
 
-export default function Login() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState(''); // useState retorna dois valores: username e um função
+  
+  function HandSubmit(e){
+    e.preventDefault(); // Bloqueia o eventDefault(Redirecionar para outra pagina) de um form
+    history.push('/main')
+    
+  }
+
   return(
     <div className="login-container">
-      <form>
+      <form onSubmit={HandSubmit}>
         <img src={logo} alt="Tindev" />
-        <input placeholder="Digite seu usuário do GitHub" />
+        <input 
+        placeholder="Digite seu usuário do GitHub" 
+        value={username}
+        onChange={e => setUsername(e.target.value)} // onChange returna um event 
+        />
         <button type="submit">Enviar</button>
       </form>
     </div>
   );
 } 
+
+// O state de um component é toda aquela informação que ele vai manipular
