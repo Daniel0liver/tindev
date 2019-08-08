5395a7ad11cd6a9cbb22adc8d2c4567342d991ab
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-08-07 22:45:24 -0300

    rendering users on my main page

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
 
 export default function({ match }) { // A propriedade match pega todos os parametros passados para aquela rota
+  const [users, setUsers] = useState([]);
+  
+  //useEffect é uma função que será exucutado quando meu component for renderizado em tela
+  useEffect(() => {
+    async function loadUsers(){
+      const response = await api.get('/devs', {
+        headers: { 
+          user: match.params.id,
+        }
+      })
+
+      setUsers(response.data);
+      
+    }
+
+    loadUsers();
+  }, [match.params.id]); // Recebe dois parametros: uma função e quando você quer executar essa função
+
   return( 
     <div className="main-container">
       <img src={logo} alt="Tindev" />
       <ul>
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/35768750?s=400&u=1fb0804013c8a8098fdf3d967ed5236dcebfba8c&v=4" alt="" />
+        {users.map(user => (
+          <li key={user._id}>
+          <img src={user.avatar} alt="" />
           <footer>
-            <strong>Daniel Oliveira</strong>
-            <p>Apreciador da tecnologia da informação, desenvolvedor. Graduando em Ciência da Computação - IFCE Maracanaú </p>
+            <strong>{user.name}</strong>
+            <p>{user.bio}</p>
           </footer>
           <div  className="buttons">
             <button type="button">
@@ -25,38 +46,8 @@ export default function({ match }) { // A propriedade match pega todos os parame
             </button>
           </div>
         </li>
-
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/35768750?s=400&u=1fb0804013c8a8098fdf3d967ed5236dcebfba8c&v=4" alt="" />
-          <footer>
-            <strong>Daniel Oliveira</strong>
-            <p>Apreciador da tecnologia da informação, desenvolvedor. Graduando em Ciência da Computação - IFCE Maracanaú </p>
-          </footer>
-          <div  className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/35768750?s=400&u=1fb0804013c8a8098fdf3d967ed5236dcebfba8c&v=4" alt="" />
-          <footer>
-            <strong>Daniel Oliveira</strong>
-            <p>Apreciador da tecnologia da informação, desenvolvedor. Graduando em Ciência da Computação - IFCE Maracanaú </p>
-          </footer>
-          <div  className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
+        ))}
+        
       </ul>
     </div>
   );
