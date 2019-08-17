00180ee4f64bb7a37b83cb0cbf8d9015e4e4f3c5
Daniel Oliveira <dan.oliveira461@gmail.com>
2019-08-17 12:07:05 -0300

    adding dependencies
    added: react-navigation, react-gesture-handler and react-reanimated
    
    configuring android MainActivity.java, adding react-gesture-handler dependencies

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -4,3 +4,16 @@ import { View } from 'react-native';
 export default function Main() {
   return <View />
 }
+
+
+/*
+ - react-navigation:
+   serve para navegação básica
+
+ - react-native-gesture-handlur:
+   é uma dependência do react navigation
+   e serve para trabalhar com gestos do usuário 
+
+ - react-reanimated:
+   animações de transição
+*/ 
